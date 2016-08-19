3973d1a125423956fa990e99bdae53c8d61b10b2
liguodong <liguodong@cd.tuan800.com>
2016-08-19 19:32:23 +0800

    Scala 连接数据库

---
+++ src/main/scala/scalacookbook/chapter16/ScalaJdbcConnectBySpring.scala
@@ -0,0 +1,37 @@
+package scalacookbook.chapter16
+
+import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.jdbc.core.support.JdbcDaoSupport
+
+
+/**
+  * You want to connect to a database using the Spring Framework.
+  * This gives you a nice way to add connection pooling and
+  * other capabilities to your SQL code.
+  *
+  * Created by liguodong on 2016/8/19.
+  */
+
+object ScalaJdbcConnectBySpring extends App{
+
+  // read the application context file
+  val ctx = new ClassPathXmlApplicationContext("ApplicationContext-db.xml",
+    "ApplicationContext-dao.xml")
+
+  // get a testDao instance
+  val userDao = ctx.getBean("userDao").asInstanceOf[UserDao]
+
+  val numUsers = userDao.getNumUsers
+  println("You have this many users: " + numUsers)
+
+}
+
+
+
+class UserDao extends JdbcDaoSupport{
+  def getNumUsers: Int = {
+    val query = "select count(1) as num from student_info"
+    val result = getJdbcTemplate.queryForMap(query)
+    return  result.get("num").asInstanceOf[Long].toInt
+  }
+}
